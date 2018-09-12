d7b6188fdfb60abd3a2416b97762dd1aa7009961
puresox <puresox@163.com>
2018-09-12 14:54:38 +0800

    cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Koa = require('koa');
-const cors = require('@koa/cors');
+const cors = require('koa2-cors');
 const bodyParser = require('koa-bodyparser');
 const serve = require('koa-static');
 const router = require('./router');
