870fb19c80dd6b36e34b73b5b2c467859e910b03
puresox <puresox@163.com>
2018-09-14 11:13:44 +0800

    io

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const Koa = require('koa');
 const cors = require('koa2-cors');
 const bodyParser = require('koa-bodyparser');
 const serve = require('koa-static');
+const http = require('http');
+const socketio = require('socket.io');
+const socket = require('./socketio.js');
 const router = require('./router');
 const { logger } = require('./logger.js');
 const { keys, port } = require('./config/config.js');
 
 const app = new Koa();
+const server = http.createServer(app.callback());
+
 app.use(cors());
 
 app.keys = keys;
@@ -21,7 +26,10 @@ app.use(
 
 app.use(router.routes());
 
-app.listen(port);
+const io = socketio(server);
+socket(io);
+
+server.listen(port);
 logger.info(`system start,listened on ${port}`);
 
 app.on('error', (err) => {
